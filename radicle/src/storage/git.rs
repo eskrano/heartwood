f9dfb2aee9899c70f8014a16902573078803c82e
Alexis Sellier <self@cloudhead.io>
2023-01-19 19:46:39 +0100

    node: Fully integrate worker fetch into service

--- radicle/src/storage/git.rs
+++ radicle/src/storage/git.rs
@@ -631,13 +631,19 @@ impl WriteRepository for Repository {
             opts.remote_callbacks(callbacks);
 
             let refspec = if let Some(namespace) = namespace {
+                // TODO: Make sure we verify before pruning, as pruning may get us into
+                // a state we can't roll back.
+                opts.prune(git2::FetchPrune::On);
+
                 format!("refs/namespaces/{namespace}/refs/*:refs/namespaces/{namespace}/refs/*")
             } else {
+                // We should not prune in this case, because it would mean that namespaces that
+                // don't exit on the remote would be deleted locally.
+                opts.prune(git2::FetchPrune::Off);
+
+                // FIXME: We need to omit our own namespace from this refspec.
                 "refs/namespaces/*:refs/namespaces/*".to_owned()
             };
-            // TODO: Make sure we verify before pruning, as pruning may get us into
-            // a state we can't roll back.
-            opts.prune(git2::FetchPrune::On);
             // Fetch from the staging copy into the canonical repo.
             remote.fetch(&[refspec], Some(&mut opts), None)?;
         }
@@ -732,15 +738,11 @@ pub mod paths {
 
 #[cfg(test)]
 mod tests {
-    use std::io::{Read, Write};
-    use std::{io, net, process, thread};
-
     use crypto::test::signer::MockSigner;
 
     use super::*;
     use crate::assert_matches;
     use crate::git;
-    use crate::rad;
     use crate::storage::refs::SIGREFS_BRANCH;
     use crate::storage::{ReadRepository, ReadStorage, RefUpdate, WriteRepository};
     use crate::test::arbitrary;
@@ -901,115 +903,6 @@ mod tests {
         refs.sort();
 
         assert_eq!(refs, vec!["refs/heads/master", "refs/rad/id"]);
-    }
-
-    #[test]
-    #[ignore]
-    // Test the remote transport using `git-upload-pack` and TCP streams.
-    // Must be run on its own, since it tries to register the remote transport, which
-    // will fail if the mock transport was already registered.
-    fn test_upload_pack() {
-        let tmp = tempfile::tempdir().unwrap();
-        let signer = MockSigner::default();
-        let remote = *signer.public_key();
-        let storage = Storage::open(tmp.path().join("storage")).unwrap();
-        let socket = net::TcpListener::bind(net::SocketAddr::from(([0, 0, 0, 0], 0))).unwrap();
-        let addr = socket.local_addr().unwrap();
-        let source_path = tmp.path().join("source");
-        let target_path = tmp.path().join("target");
-        let (source, _) = fixtures::repository(source_path);
-
-        transport::local::register(storage.clone());
-
-        let (proj, _, _) = rad::init(
-            &source,
-            "radicle",
-            "radicle",
-            git::refname!("master"),
-            &signer,
-            &storage,
-        )
-        .unwrap();
-
-        let t = thread::spawn(move || {
-            let (stream, _) = socket.accept().unwrap();
-            let repo = storage.repository(proj).unwrap();
-            // NOTE: `GIT_PROTOCOL=version=2` doesn't work.
-            let mut child = process::Command::new("git")
-                .current_dir(repo.path())
-                .arg("upload-pack")
-                .arg("--strict") // The path to the git repo must be exact.
-                .arg(".")
-                .stdout(process::Stdio::piped())
-                .stdin(process::Stdio::piped())
-                .spawn()
-                .unwrap();
-
-            let mut stdin = child.stdin.take().unwrap();
-            let mut stdout = child.stdout.take().unwrap();
-
-            let mut stream_r = stream.try_clone().unwrap();
-            let mut stream_w = stream;
-
-            let t = thread::spawn(move || {
-                let mut buf = [0u8; 1024];
-
-                while let Ok(n) = stream_r.read(&mut buf) {
-                    if n == 0 {
-                        break;
-                    }
-                    if stdin.write_all(&buf[..n]).is_err() {
-                        break;
-                    }
-                }
-            });
-            io::copy(&mut stdout, &mut stream_w).unwrap();
-
-            t.join().unwrap();
-            child.wait().unwrap();
-        });
-
-        let mut updates = Vec::new();
-        {
-            let mut callbacks = git2::RemoteCallbacks::new();
-            let mut opts = git2::FetchOptions::default();
-
-            callbacks.update_tips(|name, _, _| {
-                updates.push(name.to_owned());
-                true
-            });
-            opts.remote_callbacks(callbacks);
-
-            let target = git2::Repository::init_bare(target_path).unwrap();
-            let stream = net::TcpStream::connect(addr).unwrap();
-
-            // Register the `heartwood://` transport for this stream.
-            transport::remote::register(remote, stream.try_clone().unwrap());
-
-            // Fetch with the `heartwood://` transport.
-            target
-                .remote_anonymous(&format!("heartwood://{remote}/{proj}"))
-                .unwrap()
-                .fetch(
-                    &["refs/namespaces/*:refs/namespaces/*"],
-                    Some(&mut opts),
-                    None,
-                )
-                .unwrap();
-
-            stream.shutdown(net::Shutdown::Both).unwrap();
-
-            t.join().unwrap();
-        }
-
-        assert_eq!(
-            updates,
-            vec![
-                format!("refs/namespaces/{remote}/refs/heads/master"),
-                format!("refs/namespaces/{remote}/refs/rad/id"),
-                format!("refs/namespaces/{remote}/refs/rad/sigrefs")
-            ]
-        );
     }
 
     #[test]
